a03386eb63f091955c6560c4b057ba819f4ba3fd
lucky0011198 <68720142+lucky0011198@users.noreply.github.com>
2021-08-07 08:54:45 +0530

    adding navigation bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,267 +1,3 @@
-(function () {
-  "use strict";
-
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim();
-    if (all) {
-      return [...document.querySelectorAll(el)];
-    } else {
-      return document.querySelector(el);
-    }
-  };
-
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all);
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach((e) => e.addEventListener(type, listener));
-      } else {
-        selectEl.addEventListener(type, listener);
-      }
-    }
-  };
-
-  /**
-   * Easy on scroll event listener
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener("scroll", listener);
-  };
-
-  /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select("#navbar .scrollto", true);
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200;
-    navbarlinks.forEach((navbarlink) => {
-      if (!navbarlink.hash) return;
-      let section = select(navbarlink.hash);
-      if (!section) return;
-      if (
-        position >= section.offsetTop &&
-        position <= section.offsetTop + section.offsetHeight
-      ) {
-        navbarlink.classList.add("active");
-      } else {
-        navbarlink.classList.remove("active");
-      }
-    });
-  };
-  window.addEventListener("load", navbarlinksActive);
-  onscroll(document, navbarlinksActive);
-
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let elementPos = select(el).offsetTop;
-    window.scrollTo({
-      top: elementPos,
-      behavior: "smooth",
-    });
-  };
-
-  /**
-   * Back to top button
-   */
-  let backtotop = select(".back-to-top");
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add("active");
-      } else {
-        backtotop.classList.remove("active");
-      }
-    };
-    window.addEventListener("load", toggleBacktotop);
-    onscroll(document, toggleBacktotop);
-  }
-
-  /**
-   * Mobile nav toggle
-   */
-  on("click", ".mobile-nav-toggle", function (e) {
-    select("body").classList.toggle("mobile-nav-active");
-    this.classList.toggle("bi-list");
-    this.classList.toggle("bi-x");
-  });
-
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on(
-    "click",
-    ".scrollto",
-    function (e) {
-      if (select(this.hash)) {
-        e.preventDefault();
-
-        let body = select("body");
-        if (body.classList.contains("mobile-nav-active")) {
-          body.classList.remove("mobile-nav-active");
-          let navbarToggle = select(".mobile-nav-toggle");
-          navbarToggle.classList.toggle("bi-list");
-          navbarToggle.classList.toggle("bi-x");
-        }
-        scrollto(this.hash);
-      }
-    },
-    true
-  );
-
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
-  window.addEventListener("load", () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash);
-      }
-    }
-  });
-
-  /**
-   * Hero type effect
-   */
-  const typed = select(".typed");
-  if (typed) {
-    let typed_strings = typed.getAttribute("data-typed-items");
-    typed_strings = typed_strings.split(",");
-    new Typed(".typed", {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000,
-    });
-  }
-
-  /**
-   * Skills animation
-   */
-  let skilsContent = select(".skills-content");
-  if (skilsContent) {
-    new Waypoint({
-      element: skilsContent,
-      offset: "80%",
-      handler: function (direction) {
-        let progress = select(".progress .progress-bar", true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute("aria-valuenow") + "%";
-        });
-      },
-    });
-  }
-
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener("load", () => {
-    let portfolioContainer = select(".portfolio-container");
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: ".portfolio-item",
-      });
-
-      let portfolioFilters = select("#portfolio-flters li", true);
-
-      on(
-        "click",
-        "#portfolio-flters li",
-        function (e) {
-          e.preventDefault();
-          portfolioFilters.forEach(function (el) {
-            el.classList.remove("filter-active");
-          });
-          this.classList.add("filter-active");
-
-          portfolioIsotope.arrange({
-            filter: this.getAttribute("data-filter"),
-          });
-          portfolioIsotope.on("arrangeComplete", function () {
-            AOS.refresh();
-          });
-        },
-        true
-      );
-    }
-  });
-
-  /**
-   * Initiate portfolio lightbox
-   */
-  const portfolioLightbox = GLightbox({
-    selector: ".portfolio-lightbox",
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper(".portfolio-details-slider", {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper(".testimonials-slider", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-      },
-    },
-  });
-
-  /**
-   * Animation on scroll
-   */
-  window.addEventListener("load", () => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false,
-    });
-  });
-})();
-
-/*github............profile.........*/
-
 fetch("https://api.github.com/users/lucky0011198", {
   headers: new Headers({
     "User-agent": "Mozilla/4.0 Custom User Agent",
@@ -278,38 +14,107 @@ fetch("https://api.github.com/users/lucky0011198", {
     //document.getElementById("followers").innerHTML = data.;
     document.getElementById("followersb").innerHTML = data.followers;
     document.getElementById("public_reposb").innerHTML = data.public_repos;
+    document.getElementById(
+      "url"
+    ).innerHTML = `<a href="${data.html_url}" class="uk-button uk-button-text">Read more</a>`;
     //document.getElementById("followingb").innerHTML = data.following;
     //console.log(data);
   })
   .catch((error) => console.error(error));
 
-/*github===================repos================
-var repo = [];
-fetch("https://api.github.com/users/lucky0011198/repos", {
-  headers: new Headers({
-    "User-agent": "Mozilla/4.0 Custom User Agent",
-  }),
-})
-  .then((response) => response.json())
-  .then((data1) => {})
-  .catch((error) => console.error(error));
-*/
 $(document).ready(function () {
   $.get("https://api.github.com/users/lucky0011198/repos", function (data) {
-    var content = " <tbody>";
-    for (i = 0; i < 20; i++) {
-      content +=
-        "<tr><td>" +
-        (i + 1) +
-        "</td> <td>" +
-        data[i].name +
-        "</td><td><b>" +
-        data[i].language +
-        "</b></td></tr>";
+    var content = "";
+    for (i = 0; i < data.length; i++) {
+      console.log(data[i].pushed_at);
+
+      content += `  <div class="uk-card uk-card-primary uk-card-hover uk-card-body">
+                          <h3 class="uk-card-title"> <img src="https://img.icons8.com/material-outlined/24/000000/repository.png"/>  ${data[i].name}</h3>
+                          <p> <span> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                              <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                            </svg> forks : ${data[i].forks} </span> <span>&nbsp; &nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hdd-rack" viewBox="0 0 16 16">
+                              <path d="M4.5 5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zM3 4.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm2 7a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm-2.5.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
+                              <path d="M2 2a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h1v2H2a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-1a2 2 0 0 0-2-2h-1V7h1a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm13 2v1a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm0 7v1a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-1a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm-3-4v2H4V7h8z"/>
+                            </svg> branch: ${data[i].default_branch} </span> <span>&nbsp; &nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-binoculars" viewBox="0 0 16 16">
+                              <path d="M3 2.5A1.5 1.5 0 0 1 4.5 1h1A1.5 1.5 0 0 1 7 2.5V5h2V2.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5v2.382a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V14.5a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 14.5v-3a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5v3A1.5 1.5 0 0 1 5.5 16h-3A1.5 1.5 0 0 1 1 14.5V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V2.5zM4.5 2a.5.5 0 0 0-.5.5V3h2v-.5a.5.5 0 0 0-.5-.5h-1zM6 4H4v.882a1.5 1.5 0 0 1-.83 1.342l-.894.447A.5.5 0 0 0 2 7.118V13h4v-1.293l-.854-.853A.5.5 0 0 1 5 10.5v-1A1.5 1.5 0 0 1 6.5 8h3A1.5 1.5 0 0 1 11 9.5v1a.5.5 0 0 1-.146.354l-.854.853V13h4V7.118a.5.5 0 0 0-.276-.447l-.895-.447A1.5 1.5 0 0 1 12 4.882V4h-2v1.5a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V4zm4-1h2v-.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5V3zm4 11h-4v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14zm-8 0H2v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14z"/>
+                            </svg> watchers : ${data[i].watchers} </span></p>
+                          <span class="uk-card-badge uk-label"><p>${data[i].language}</p></span>
+                      </div><br> `;
     }
-    content += " <tbody>";
-    console.log(content);
+
+    //console.log(content);
     $(".repos").append(content);
-    $("td").cs;
+    // $("td").cs;
   });
+  var conte = "";
+  $.get("https://api.github.com/users/lucky0011198/followers", function (data) {
+    var v = 1;
+    var conten = "";
+
+    for (i = 0; i < data.length; i++) {
+      conten += `<article class="uk-comment">
+                          <header class="uk-comment-header">
+                              <div class="uk-grid-medium uk-flex-middle" uk-grid>
+                                  <div class="uk-width-auto">
+                                      <img class="uk-comment-avatar" src="${data[i].avatar_url}" width="80" height="80" alt="">
+                                  </div>
+                                  <div class="uk-width-expand">
+                                      <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="#">${data[i].login}</a></h4>
+                                      <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
+
+                                          <li><a href="${data[i].html_url}">Read more  <i class="bi bi-arrow-up-right-square"></i> </a></li>
+                                      </ul>
+                                  </div>
+                              </div>
+                              <p style="width:100%;height:2px;background-color:#c8c6c6">
+                          </header>
+                          <div class="uk-comment-body">
+                              <b> </b>
+                          </div>
+                      </article>`;
+    }
+
+    $(".followers").append(conten);
+  });
+
+  var html = "";
+  $.get(
+    `https://api.github.com/users/lucky0011198/received_events`,
+    function (data) {
+      for (var i = 0; i < data.length; i++) {
+        html += `  <div>
+                      <div class="uk-card uk-card-primary uk-card-body">
+
+                          <h3 class="uk-card-title"><img class="uk-comment-avatar" src="${data[i].actor.avatar_url}" width="50" height="50" alt=""> &nbsp;${data[i].actor.login}</h3>
+                          <a class="" href="#">  <img src="https://img.icons8.com/material-outlined/24/000000/repository.png"/> Repositories</a>
+                                  <div class=""><p>  ${data[i].repo.name}</p>
+                                      <p> </p>
+                                  </div>
+
+<p>Type : ${data[i].type}</p>
+                      </div>
+                  </div>`;
+      }
+
+      $(".event").append(html);
+    }
+  );
+
+  $(".drower").click(function () {
+    var div = $(".drower");
+    div.animate({ top: "500px", width: "300px", opacity: "0.8" }, "slow");
+    div.animate({ top: "500px", height: "300px", opacity: "0.4" }, "slow");
+  }),
+    $(".float-action-button").click(function () {
+      var div = $(".drower");
+      div.animate({ top: "90%", height: "50px", opacity: "0.4" }, "slow");
+      div.animate({ top: "90%", width: "50px", opacity: "0.8" }, "slow");
+    });
 });
+function changecolor() {
+  var color = document.getElementById("color").value;
+  document.getElementById("head").style.backgroundColor = color;
+}
+
+//floating action button////////////
